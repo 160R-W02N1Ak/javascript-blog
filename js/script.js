20ad274d823e47c651acac55f5be3ef4e1c685b8
Igor Woźniak <igor91wozniak@gmail.com>
2020-12-14 19:18:09 +0100

    add active function for articles and links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -7,12 +9,25 @@
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log (event);
+
+
   
-    /* remove class 'active' from all article links  */
+    /*  remove class 'active' from all article links  [DONE] */  
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   
     /* add class 'active' to the clicked link */
   
-    /* remove class 'active' from all articles */
+    /*  remove class 'active' from all articles  [DONE] */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+    
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
   
     /* get 'href' attribute from the clicked link */
   
